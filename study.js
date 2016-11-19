79228412393f8a8766d624259046c32ddcda5f80
watayuz <w-desu@i.softbank.jp>
2016-11-19 23:15:28 +0900

    add about callback and length

--- study.js
+++ study.js
@@ -9,8 +9,45 @@ var Person = {
 };
 
 Person.getName = function() {
-    return this.name;
+    return this.name;   // "tarou"
+    return name;        // undefined
 };
 
 console.log(Person.getAge());
 console.log(Person.getName());
+
+/* Callback */
+var doCallback = function(callback) {
+    callback('done');
+}
+doCallback(function(result) {
+    console.log('result: ' + result);
+});
+
+var doCallback2 = function(callback) {
+    if (callback && typeof callback === 'function')callback('done2');
+}
+doCallback2(function(result) {
+    console.log('result: ' + result);
+});
+
+/* Cache the length */
+//NO
+var foo = document.getElementById('p');
+if (foo != null && foo.length < 1){
+    for (var i=0; i<foo.length; i++){ 
+        console.log(i);
+    }
+} else {
+    console.log('null ppoi');
+}
+
+// YES
+var foo2 = document.getElementById('p');
+if (foo != null && foo.length < 1){
+    for (var i=0, len=foo2.length; i< len; i++){
+        console.log(i);
+    }
+} else {
+    console.log('null ppoi');
+}
